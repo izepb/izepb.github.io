3cbd26cd37272855fd375218841c55bc04bbaaf3
Pisanu Buphamalai <pbuphamalai@cemm.oeaw.ac.at>
2021-05-02 18:19:50 +0200

    edited logo

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:/Users/pbuphamalai/Documents/projects/personalwebsite/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/assets/js/wowchemy-utils.js
+  // ns-hugo:/Users/pbuphamalai/Documents/projects/GitHubPage/content-editor/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
